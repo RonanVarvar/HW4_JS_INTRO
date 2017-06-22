e56a1368ce353b7100fa5c7b68409fe4c7ad9751
Roman Veselovsky <romanveselovsky@gmail.com>
2017-06-22 21:00:45 +0300

    HW4-JS-Intro Add new file's to repository

---
+++ script.js
@@ -0,0 +1,101 @@
+/*var a = +prompt('введіть число a');
+var b = +prompt('введіть число b');
+var result = a + b;
+console.log (result);
+
+var a = +prompt('введіть число a');
+var b = +prompt('введіть число b');
+var result = a + b;
+console.log (result);
+
+var a = +prompt('введіть число a');
+var b = +prompt('введіть число b');
+var result = a + b;
+console.log (result);
+
+var a = +prompt('input a');
+var b = +prompt('input b');
+var result = a + b;
+console.log (result);
+
+var a = +prompt('введіть число a');
+var b = +prompt('введіть число b');
+var c = +prompt('введіть число c');
+var res1 = -b/2/a+Math.pow(Math.pow(b,2)-4*a*c,0.5)/2/a;
+var res2 = -b/2/a-Math.pow(Math.pow(b,2)-4*a*c,0.5)/2/a;
+if (isNaN(res1) && isNaN(res2)) {
+	console.log('Немає розвязків рівняння');
+	}  else {
+	console.log('x1 = ' + res1, 'x2 = ' +res2);
+}
+*/
+
+if (confirm ('Чи бажаєте почати гру?')) { 
+sessionStorage.clear();
+check: 
+	while  (true) {
+	var a = Math.random()*5;
+	var rand = Math.floor(a);
+	var first = +prompt('У вас є три спроби, щоб вгадати ціле число від 0 до 5. Введіть число:');
+	var second = +prompt('Друга спроба. Введіть число:');
+	var third = +prompt('Третя спроба. Введіть число:');
+	switch (rand) {
+		case first:
+		var j =  +sessionStorage.getItem ('prize');
+		if (j > 0) {
+			var i = +30;
+			}
+			else {
+				i = +10;
+			}
+		var prize = sessionStorage.setItem('prize', j+i);				
+		if (confirm('Ви виграли, чи хочете продовжити гру?')) {
+			continue check;
+			} else {
+			console.log ('Дякуємо за гру, ви виграли ' + sessionStorage.getItem('prize')+ '$');
+			break check;
+			}
+		case second:
+		var j =  +sessionStorage.getItem ('prize');
+		if (j > 0) {
+			var i = +15;
+			}
+			else {
+				i = +5;
+			}
+		var prize = sessionStorage.setItem('prize', j+i);		
+		if (confirm('Ви виграли, чи хочете продовжити гру?')) {
+			continue check;
+			} else {
+			console.log ('Дякуємо за гру, ви виграли ' + sessionStorage.getItem('prize')+ '$');
+			break check;
+			}
+		case third:
+		var j =  +sessionStorage.getItem ('prize')
+		if (j > 0) {
+			var i = +6;
+			}
+			else {
+				i = +2;
+			}
+		var prize = sessionStorage.setItem('prize', j+i);		
+		if (confirm('Ви виграли, чи хочете продовжити гру?')) {
+			continue check;
+			} else {
+			console.log ('Дякуємо за гру, ви виграли ' + sessionStorage.getItem('prize')+ '$');
+			break check;
+			}
+		default:
+		if (confirm ('Ваш виграш 0$. Чи бажаєте зіграти ще раз?')) {
+			continue check;
+			} 
+			else { 
+			console.log ('Дякуємо за гру, ви виграли ' + Number(sessionStorage.getItem('prize'))+ '$');
+		    break check;
+			}
+		}
+	}
+}  else {
+	console.log ('Сьогодні ви не виграли мільйон, а могли');
+}
+		
